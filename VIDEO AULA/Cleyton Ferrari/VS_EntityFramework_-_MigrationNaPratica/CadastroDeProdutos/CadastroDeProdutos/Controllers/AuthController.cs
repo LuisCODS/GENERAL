c5a0a19d26353eaba6cbfc76a0b046975cb6497b
LuisCODS <tennis.bresil@gmail.com>
2018-04-20 10:01:07 -0400

    a

--- VIDEO AULA/Cleyton Ferrari/VS_EntityFramework_-_MigrationNaPratica/CadastroDeProdutos/CadastroDeProdutos/Controllers/AuthController.cs
+++ VIDEO AULA/Cleyton Ferrari/VS_EntityFramework_-_MigrationNaPratica/CadastroDeProdutos/CadastroDeProdutos/Controllers/AuthController.cs
@@ -4,6 +4,8 @@ using CadastroDeProdutos.ModelsViews;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -14,8 +16,42 @@ namespace DemoAuthentication.Controllers
     {
         ProdutoContexto db = new ProdutoContexto();
 
-        // ============================= Login ==================================
-        public ActionResult Login() 
+        // ============================= CREATE ==================================
+        public ActionResult Create()
+        {
+            return View(new Utilisateur());
+        }
+        [HttpPost]
+        public ActionResult Create(Utilisateur user)
+        {
+            if (ModelState.IsValid)
+            {
+                string password = user.PasswordRepeated;
+                byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+                byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+                string encoded = BitConverter.ToString(hash);
+                user.PasswordRepeated = encoded;
+
+                db.Utilisateurs.Add(user);
+                db.SaveChanges();
+                FormsAuthentication.SetAuthCookie(user.UtilisateurID.ToString(), false);
+                return RedirectToAction("Index", "Home");
+            }
+            //return utilisateur.Id;
+            return View(user);
+        }
+
+        //public string HashMotDePasse()
+        //{
+            
+        //    get { return _hashMotDePasse; }
+        //    set{
+        //        _hashMotDePasse = PasswordHash.PasswordHash.CreateHash(value);
+        //    }
+        //}
+
+// ============================= Login ==================================
+        public ActionResult Login()
         {
             return View();
         }
@@ -26,13 +62,13 @@ namespace DemoAuthentication.Controllers
             if (IsValid(userModel))
             {
                 FormsAuthentication.SetAuthCookie(userModel.NomUtilisateur, userModel.Persistant);
-                if(!string.IsNullOrWhiteSpace(ReturnUrl) && Url.IsLocalUrl(ReturnUrl))
+                if (!string.IsNullOrWhiteSpace(ReturnUrl) && Url.IsLocalUrl(ReturnUrl))
                     return Redirect(ReturnUrl);
                 return Redirect("/");
             }
             else
             {
-                ModelState.AddModelError("","Le nom d'utilisateur ou le mot de passe est incorrect. ");
+                ModelState.AddModelError("", "Le nom d'utilisateur ou le mot de passe est incorrect. ");
                 return View(userModel);
             }
 
@@ -41,13 +77,13 @@ namespace DemoAuthentication.Controllers
         private bool IsValid(LoginModel user)
         {
             Utilisateur userFromBd = db.Utilisateurs.FirstOrDefault(u => u.NomUtilisateur == user.NomUtilisateur && u.Password == user.Password);
-            if (userFromBd != null)            
-                return (true);            
+            if (userFromBd != null)
+                return (true);
             return (false);
         }
 
         // ============================= Logout ==================================
-        public ActionResult Logout() 
+        public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
             return Redirect("/");
