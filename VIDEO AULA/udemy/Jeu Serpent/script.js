788264c4421be37bd06d1d80bff49f3cdbd15201
LuisCODS <tennis.bresil@gmail.com>
2018-04-29 02:21:10 -0400

    k

--- VIDEO AULA/udemy/Jeu Serpent/script.js
+++ VIDEO AULA/udemy/Jeu Serpent/script.js
@@ -4,5 +4,10 @@ window.onload = function()
     canvas.width = 900;
     canvas.height = 600;
     canvas.style.border = "1px solid";
-    document.boody.appendChild(canvas);     
+    document.boody.appendChild(canvas);   
+    
+    var cxt = canvas.getContext('2d');
+    cxt.fillStyle = "#ff0000";
+    cxt.fillRect(30, 30, 100, 50);
+    
 }
